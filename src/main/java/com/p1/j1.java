f7ada22d501cb3cbb22eb82954e3fe4a06da0836
ganeshseeram123 <ganeshseeram5decem@gmail.com>
2025-09-05 17:29:04 -0400

    Adding j1 and j2 files

---
+++ src/main/java/com/p1/j1.java
@@ -0,0 +1,14 @@
+package com.p1;
+
+public class j1
+{
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+        for (int i = 6; i <= 10; i++)
+        {
+            System.out.println("j = " + i);
+        }
+    }
+}
